85c0709f58db750855c9ea77b5aba5e9a33fb7a2
mr poe <127251376+aidanpoe@users.noreply.github.com>
2025-10-20 18:23:56 +0100

    Update copyright year to 2025

--- lua/star_trek/industrial_replicator/sv_database.lua
+++ lua/star_trek/industrial_replicator/sv_database.lua
@@ -1,19 +1,19 @@
 ---------------------------------------
 ---------------------------------------
---         Star Trek Modules         --
---                                   --
---            Created by             --
+--        made for Star Trek Module  --
+-- Addon Created by HouseofPoe.co.uk --
+--        API Created by             --
 --       Jan 'Oninoni' Ziegler       --
 --                                   --
 -- This software can be used freely, --
 --    but only distributed by me.    --
 --                                   --
---    Copyright © 2022 Jan Ziegler   --
+--    Copyright © 2025 Jan Ziegler   --
 ---------------------------------------
 ---------------------------------------
 
 ---------------------------------------
--- Industrial Replicator | Database  --
+-- Star Trek Transport Relay | Loader--
 ---------------------------------------
 
 Star_Trek.IndustrialReplicator.Database = Star_Trek.IndustrialReplicator.Database or {}

2d3b93b557df0387dff7446d4427acc22a992975
mr poe <127251376+aidanpoe@users.noreply.github.com>
2025-10-20 18:28:50 +0100

    Update copyright year in shared.lua
    Updated copyright year from 2022 to 2025.

--- lua/star_trek/industrial_replicator/entities/industrial_replicator_entity/shared.lua
+++ lua/star_trek/industrial_replicator/entities/industrial_replicator_entity/shared.lua
@@ -1,19 +1,15 @@
 ---------------------------------------
 ---------------------------------------
---         Star Trek Modules         --
---                x                   --
---            Created by             --
+--        made for Star Trek Module  --
+-- Addon Created by HouseofPoe.co.uk --
+--        API Created by             --
 --       Jan 'Oninoni' Ziegler       --
 --                                   --
 -- This software can be used freely, --
 --    but only distributed by me.    --
 --                                   --
---    Copyright © 2022 Jan Ziegler   --
+--    Copyright © 2025 Jan Ziegler   --
 ---------------------------------------
----------------------------------------
-
----------------------------------------
---  Industrial Replicator | Shared   --
 ---------------------------------------
 
 if not istable(ENT) then Star_Trek:LoadAllModules() return end
